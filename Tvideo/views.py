9bb0cb08ad9904e1f78e7c06ee00ae786f81b184
Khadimou <khadimou-rassoul.diop@ens.uvsq.fr>
2023-05-24 11:26:47 +0200

    mvp transcribing and translating from a language to another

--- Tvideo/views.py
+++ Tvideo/views.py
@@ -2,39 +2,47 @@ from django.shortcuts import render
 from .forms import FichierForm
 from .models import Fichier
 from .utils import transcribe, translate_txt, generate_audio
-from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def accueil(request):
     return render(request, 'accueil.html')
 
 def resultat(request):
-    fichiers = Fichier.objects.all()
-    return render(request, 'resultat.html', {'fichiers': fichiers})
+    audio_url = request.GET.get('audio_url')
+    fichiers = Fichier.objects.filter(fichier_audio__isnull=False)
+    return render(request, 'resultat.html', {'fichiers': fichiers, 'audio_url': audio_url})
+
+from django.shortcuts import redirect
+from django.http import QueryDict
+from django.urls import reverse
 
 def telechargement_vue(request):
     if request.method == 'POST':
         form = FichierForm(request.POST, request.FILES)
-        print(form.errors)
         if form.is_valid():
             fichier = request.FILES['fichier']
-            
             # Enregistrez le fichier ou faites d'autres opérations nécessaires
             texte_traduit = transcribe(fichier)
             text = translate_txt(texte_traduit, "fr")
             # Appliquez le traitement au fichier
-            fichier_audio = generate_audio(text, "fr", "generated_audio.mp3")
-            
-            fichier_obj = Fichier()
-            fichier_obj.fichier_audio = fichier_audio
+            audio = generate_audio(text, "fr", "generated_audio.mp3")
+
+            # Enregistrez l'audio généré dans la base de données
+            fichier_obj = Fichier.objects.create(fichier_audio=audio)
             fichier_obj.save()
 
-            # Obtenir l'URL complète du fichier audio
-            url = request.build_absolute_uri()
+            # Récupérez l'URL de l'audio généré
+            audio_url = fichier_obj.fichier_audio.url
 
-            # Renvoyer la réponse HTTP
-            return render(request, 'resultat.html', {'fichiers': [fichier_obj], 'audio_url': url})
+            # Ajoutez l'URL de l'audio généré comme paramètre de requête dans l'URL de redirection
+            query_string = QueryDict(mutable=True)
+            query_string['audio_url'] = audio_url
+            redirect_url = reverse('resultat') + '?' + query_string.urlencode()
 
+            # Redirigez vers la vue resultat avec l'URL de l'audio généré comme paramètre de requête
+            return redirect(redirect_url)
     else:
         form = FichierForm()
+
     return render(request, 'telechargement.html', {'form': form})
 
